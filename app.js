918542eb45ffbf96943d9465a9a6e10fb442b671
Armandas G <genysarmandas@gmail.com>
2023-10-02 21:53:38 +0300

    express static public repo

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 // express app
 const app = express();
@@ -11,13 +12,18 @@ app.set('views', 'views')
 // listen for requests
 app.listen(3000);
 
-app.use((req, res, next) => {
-    console.log('new request made:');
-    console.log('host: ', req.hostname);
-    console.log('host: ', req.path);
-    console.log('host: ', req.method);
-    next();
-});
+// middleware & static files
+app.use(express.static('public'));
+
+app.use(morgan('dev'))
+
+// app.use((req, res, next) => {
+//     console.log('new request made:');
+//     console.log('host: ', req.hostname);
+//     console.log('host: ', req.path);
+//     console.log('host: ', req.method);
+//     next();
+// });
 
 
 
@@ -32,10 +38,10 @@ res.render('index', { title: 'Home', blogs });
 
 });
 
-app.use((req, res, next) => {
-    console.log('in the next middleware');
-    next();
-});
+// app.use((req, res, next) => {
+//     console.log('in the next middleware');
+//     next();
+// });
 
 app.get('/about', (req, res) => {
 
