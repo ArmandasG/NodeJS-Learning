d7160d8c1f062244655a16624844fe3b2b4e22ca
Armandas G <genysarmandas@gmail.com>
2023-10-05 22:33:41 +0300

    get / post / delete

--- app.js
+++ app.js
@@ -24,6 +24,9 @@ app.set('views', 'views')
 // middleware & static files
 app.use(express.static('public'));
 
+// for forms that are in create
+app.use(express.urlencoded({extended: true}));
+
 app.use(morgan('dev'))
 
 // mongoose and mongo sandbox routes
@@ -108,10 +111,47 @@ res.render('index', { title: 'All Blogs', blogs: result })
         })
 })
 
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body)
+
+    blog.save()
+        .then((result) => {
+            res.redirect('/blogs')
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
 
 app.get('/blogs/create', (req, res) => {
-res.render('create', { title: 'Create a new Blog' });
+    res.render('create', { title: 'Create a new Blog' });
+    })
+    
+
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+
+    Blog.findByIdAndDelete(id)
+        .then((result) => {
+            res.json({ redirect: '/blogs' })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 })
+
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+        .then(result => 
+            res.render('details', {blog: result, title: 'Blog Details'})
+        )
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+
 
 // 404 page - veikia visiems atvejams, nereikia nurodyti path,
 // Kadangi express veikimo principas iki kol pasiekia sąlyga ir toliau nevykdo kodo
